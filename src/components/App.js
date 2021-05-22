20058e60e9765441e9efa1a2854bc7eeaf941173
yassar123 <yassarali1999@gmail.com>
2021-05-22 14:33:54 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,11 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <div id="root">
+        <p>I am learning React. My life is getting better.</p>
+      </div>
+    </div>
   )
 }
 
